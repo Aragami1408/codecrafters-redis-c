947534330fb4013c845b8da3cdfa689975a19c2b
Aragami1408 <vucaominh1408@gmail.com>
2024-07-14 16:04:26 +1000

    Move all includes to server.h

--- app/main.c
+++ app/main.c
@@ -1,23 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <string.h>
-#include <errno.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <strings.h>
-#include <sys/time.h>
-#include <time.h>
-#include <inttypes.h>
-#include <stdint.h>
-#include <math.h>
 #include "server.h"
 
-
-typedef struct
-{
+typedef struct {
 	char key[128];
 	char value[128];
 	uint64_t expiry_time;
--- app/server.h
+++ app/server.h
@@ -1,6 +1,21 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <strings.h>
+#include <sys/time.h>
+#include <time.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <math.h>
 #include "utils.h"
 
 #define BUFFER_SIZE 1024
