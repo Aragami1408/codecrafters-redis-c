77b948f6b0dcc3574dc6a069f275602737eaa7c6
Aragami1408 <vucaominh1408@gmail.com>
2024-07-14 12:27:58 +1000

    Refactored server.c

--- app/server.c
+++ app/server.c
@@ -15,36 +15,31 @@
 #include <math.h>
 
 #define BUFFER_SIZE 1024
+#define COMMAND_MAX_LENGTH 15
+#define BUCKET_SIZE 10000
 
 typedef struct
 {
 	char key[128];
 	char value[128];
-	uint64_t is_expired; // either 1 or 0
 	uint64_t expiry_time;
 } field;
 
 typedef struct {
-	field *bucket[10000];
+	field *bucket[BUCKET_SIZE];
 	size_t map_size;
 } map;
 
-static map global_map;
-
+static map global_map = {.map_size = 0};
 static uint64_t time_since_set_command = 0;
 static uint64_t time_since_get_command = 0;
 
-// the thread callback
+
 void *connection_handler(void *);
-
-// RESP parsing function
 void parse_resp(char *message, size_t length, char *output);
-
-// Get current time in milliseconds
 uint64_t get_current_time();
 
-int main()
-{
+int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
 	setbuf(stderr, NULL);
@@ -52,16 +47,10 @@ int main()
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	printf("Logs from your program will appear here!\n");
 
-	// Setup default values for hash table
-	// hmput(hash, "foo", "bar");
-	// hmput(hash, "baz", "qux");
-
-	int server_fd, client_addr_len;
+	int server_fd, client_fd, client_addr_len;
 	struct sockaddr_in client_addr;
-
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1)
-	{
+	if (server_fd == -1) {
 		printf("Socket creation failed: %s...\n", strerror(errno));
 		return 1;
 	}
@@ -69,8 +58,7 @@ int main()
 	// Since the tester restarts your program quite often, setting SO_REUSEADDR
 	// ensures that we don't run into 'Address already in use' errors
 	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
-	{
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
 		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
 		return 1;
 	}
@@ -81,30 +69,25 @@ int main()
 		.sin_addr = {htonl(INADDR_ANY)},
 	};
 
-	if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0)
-	{
+	if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
 		printf("Bind failed: %s \n", strerror(errno));
 		return 1;
 	}
 
 	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0)
-	{
+	if (listen(server_fd, connection_backlog) != 0) {
 		printf("Listen failed: %s \n", strerror(errno));
 		return 1;
 	}
 
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
-
 	pthread_t thread_id;
-	int client_fd;
-	while ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len)))
-	{
-		printf("Client connected\n");
 
-		if (pthread_create(&thread_id, NULL, connection_handler, (void *)&client_fd) < 0)
-		{
+	while ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len))) {
+		puts("Client connected");
+
+		if (pthread_create(&thread_id, NULL, connection_handler, (void *)&client_fd) < 0) {
 			perror("Could not create thread");
 			return 1;
 		}
@@ -112,14 +95,12 @@ int main()
 		puts("Handler assigned");
 	}
 
-	if (client_fd < 0)
-	{
+	if (client_fd < 0) {
 		printf("Accept failed: %s\n", strerror(errno));
 		return 1;
 	}
 
 	close(server_fd);
-	close(client_fd);
 	return 0;
 }
 
@@ -127,30 +108,28 @@ void *connection_handler(void *fd)
 {
 	// Get the socket descriptor
 	int client_fd = *(int *)fd;
-
 	char client_message[BUFFER_SIZE];
 	int bytes_received;
-	while ((bytes_received = recv(client_fd, client_message, BUFFER_SIZE, 0)) > 0)
-	{
+
+	while ((bytes_received = recv(client_fd, client_message, BUFFER_SIZE, 0)) > 0) {
 		client_message[bytes_received] = '\0';
 		printf("%s\n", client_message);
 
 		char response[100];
 		parse_resp(client_message, bytes_received, response);
 
-		int bytes_sent = send(client_fd, response, strlen(response), 0);
+		send(client_fd, response, strlen(response), 0);
 	}
 
-	if (bytes_received == 0)
-	{
+	if (bytes_received == 0) {
 		printf("Client disconnected\n");
 		fflush(stdout);
 	}
-	else if (bytes_received == -1)
-	{
+	else if (bytes_received == -1) {
 		printf("Receive failed: %s\n", strerror(errno));
 	}
 
+	close(client_fd);
 	return 0;
 }
 
@@ -159,27 +138,23 @@ void parse_resp(char *message, size_t length, char *output)
 #define COMMAND_MAX_LENGTH 15
 
 	int array_len = (int)strtok(message, "\r\n")[1] - '0';
-
 	char commands[array_len + 1][COMMAND_MAX_LENGTH];
 
-	for (int i = 0; i < array_len; i++)
-	{
-		int command_length = (int)strtok(0, "\r\n")[1] - '0';
-		strncpy(commands[i], strtok(0, "\r\n"), command_length);
+	for (int i = 0; i < array_len; i++) {
+		int command_length = (int)strtok(NULL, "\r\n")[1] - '0';
+		strncpy(commands[i], strtok(NULL, "\r\n"), command_length);
 		commands[i][command_length] = '\0';
 	}
 
 	if (strcasecmp(commands[0], "PING") == 0)
-	{
 		strcpy(output, "+PONG\r\n");
-	}
-	else if (strcasecmp(commands[0], "ECHO") == 0)
-	{
+	else if (strcasecmp(commands[0], "ECHO") == 0) {
 		if (commands[1] != NULL) {
-			char *result = (char *)malloc(100 * sizeof(char));
-			snprintf(result, 99, "$%zu\r\n%s\r\n", strlen(commands[1]), commands[1]);
-			strcpy(output, result);
-			free(result);
+			snprintf(output, BUFFER_SIZE, "$%zu\r\n%s\r\n", strlen(commands[1]), commands[1]);
+			// char *result = (char *)malloc(100 * sizeof(char));
+			// snprintf(result, 99, "$%zu\r\n%s\r\n", strlen(commands[1]), commands[1]);
+			// strcpy(output, result);
+			// free(result);
 		}
 		else {
 			strcpy(output, "-SYNTAXERROR No parameter supplied for 'ECHO' command\r\n");
@@ -188,7 +163,6 @@ void parse_resp(char *message, size_t length, char *output)
 	else if (strcasecmp(commands[0], "SET") == 0)
 	{
 		field *f = (field *)malloc(sizeof(field));	
-		f->is_expired = 0;	
 		if (array_len == 3) {
 			strcpy(f->key, commands[1]);
 			strcpy(f->value, commands[2]);
@@ -202,20 +176,25 @@ void parse_resp(char *message, size_t length, char *output)
 			}
 			else {
 				strcpy(output, "-SYNTAXERROR Wrong format for 'SET' command\r\n");
+				free(f);
+				return;
 			}
 		}
 		else {
 			strcpy(output, "-SYNTAXERROR Insufficient parameters for 'SET' command\r\n");
+			free(f);
+			return;
 		}
 
 		global_map.bucket[global_map.map_size++] = f;
 		strcpy(output, "+OK\r\n");
-		free(f);
 	}
 	else if (strcasecmp(commands[0], "GET") == 0)
 	{
-		field *f = (field *)malloc(sizeof(field));
 		char *key = commands[1];
+		// field *f = (field *)malloc(sizeof(field));
+		field *f = NULL;
+
 		for (int i = 0; i < global_map.map_size; i++) {
 			if (strcmp(global_map.bucket[i]->key, key) == 0) {
 				f = global_map.bucket[i];
@@ -223,18 +202,16 @@ void parse_resp(char *message, size_t length, char *output)
 			}
 		}
 
-		if (f->value != NULL)
+		//if (f->value != NULL)
+		if (f != NULL && f->value[0] != '\0')
 		{
-			char *value = f->value;
 			uint64_t expiry_time = f->expiry_time;
-			char *result = (char *)malloc(100 * sizeof(char));
 
 			if (expiry_time != 0) {
 				printf("The key %s will expire at %zu milliseconds\n", key, expiry_time);
 				time_since_get_command = get_current_time();
 				if ((time_since_get_command - time_since_set_command) < expiry_time) {	
-					snprintf(result, 99, "$%zu\r\n%s\r\n", strlen(value), value);
-					strcpy(output, result);
+					snprintf(output, BUFFER_SIZE, "$%zu\r\n%s\r\n", strlen(f->value), f->value);
 				}
 				else {
 					strcpy(output, "$-1\r\n");
@@ -242,35 +219,22 @@ void parse_resp(char *message, size_t length, char *output)
 			}
 			else {
 				printf("The key %s never expire\n", key);
-				snprintf(result, 99, "$%zu\r\n%s\r\n", strlen(value), value);
-				strcpy(output, result);
+				snprintf(output, BUFFER_SIZE, "$%zu\r\n%s\r\n", strlen(f->value), f->value);
 			}
-
-			free(result);
 		}
 		else
 		{
 			strcpy(output, "$-1\r\n");
 		}
-		free(f);
 	}
 	else
 	{
 		strcpy(output, "-SYNTAXERROR unknown command\r\n");
 	}
-
-#undef COMMAND_LENGTH
 }
 
 uint64_t get_current_time() {
-	uint64_t ms;
-	time_t s;
 	struct timespec spec;
-
-	clock_gettime(CLOCK_REALTIME, &spec);
-
-	s = spec.tv_sec;
-	ms = round(spec.tv_nsec / 1.0e6);
-
-	return ms;
+    clock_gettime(CLOCK_REALTIME, &spec);
+    return spec.tv_sec * 1000 + round(spec.tv_nsec / 1.0e6);
 }
